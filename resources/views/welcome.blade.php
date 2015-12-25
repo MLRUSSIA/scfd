389643417ff28d106033f5c1aebdb619a987729a
ml-se <MLRUSSIA>
2015-12-25 10:56:06 +0300

    Первичная верстка

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Стартовая страница</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
-                    {{ method_field('DELETE') }}
+                    Контент для не заристрированных пользователей!
                 </div>
             </div>
         </div>
