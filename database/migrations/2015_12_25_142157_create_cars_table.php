71d7c16e9801f477b589362c6a4d2e293c336bb8
ml-se <MLRUSSIA>
2015-12-25 18:47:54 +0300

    Автомобили

---
+++ database/migrations/2015_12_25_142157_create_cars_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('mark');
+            $table->string('model');
+            $table->string('year');
+            $table->string('register_number');
+            $table->string('color');
+            $table->string('sts');
+            $table->string('status');
+            $table->string('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cars');
+    }
+}
